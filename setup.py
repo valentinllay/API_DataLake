98fdd10aa800309c37a53ef7dde65bf5e2205eb3
valentinllay <143599341+valentinllay@users.noreply.github.com>
2025-05-09 19:43:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
- name="hello_world",
- version="0.1.0",
- packages=find_packages(where="src"),
- package_dir={"": "src"},
- install_requires=[
- # ajoute ici Flask si besoin, ex. "Flask>=2.2.5"
- ],
+    name="hello_world",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # par exemple "Flask>=2.2.5" si tu en as besoin
+    ],
 )
