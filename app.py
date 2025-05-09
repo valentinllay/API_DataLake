b9d1503b266f24675e966585d3579702af7119f7
valentinllay <143599341+valentinllay@users.noreply.github.com>
2025-05-09 11:18:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
+from hello_world.greet import say_hello_world
+
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def hello():
-    return "Hello World"
+ message = say_hello_world()
+ return jsonify({"message": message})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+ app.run(host="0.0.0.0", port=5000)
