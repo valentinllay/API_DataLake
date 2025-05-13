eded942ba4060dd12215dc4908b57e1364983329
valentinllay <143599341+valentinllay@users.noreply.github.com>
2025-05-13 16:10:08 +0200

    Update ApplicationStop.sh

--- deployment/ApplicationStop.sh
+++ deployment/ApplicationStop.sh
@@ -6,3 +6,9 @@ pkill -f "python app.py" || true
 
 # 2) Nettoyer l’ancien répertoire
 rm -rf /home/ubuntu/API_DataLake/
+
+# 3) Vider le cache APT
+apt-get clean
+
+# 4) Supprimer tous les logs système
+rm -rf /var/log/*
