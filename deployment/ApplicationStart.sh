bbc2af99bbbc0291c9fe45394e97397a118a4c4d
valentinllay <143599341+valentinllay@users.noreply.github.com>
2025-05-15 11:46:58 +0200

    Update ApplicationStart.sh

--- deployment/ApplicationStart.sh
+++ deployment/ApplicationStart.sh
@@ -3,11 +3,13 @@ set -e
 
 # 1) Installer Python & venv
 apt-get update -y
-apt-get install -y python3 python3-venv python3-distutils
+apt-get install -y software-properties-common
+add-apt-repository -y ppa:deadsnakes/ppa
+apt-get install -y python3.8 python3.8-venv python3.8-distutils
 
 # 2) Créer & activer venv
 cd /home/ubuntu/API_DataLake/ || { echo "Dossier introuvable"; exit 1; }
-python3 -m venv env
+python3.8 -m venv env
 . env/bin/activate
 pip install --upgrade pip
 pip install -r requirements-prod.txt
